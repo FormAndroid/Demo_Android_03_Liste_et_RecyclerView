3607736e351320040771b71294ab26921d617e86
Aurelien <aurelien.strimelle@bstorm.be>
2021-05-20 07:16:12 -0700

    Initial commit

---
+++ app/src/main/java/be/bxl/formation/demo_03_liste/DemoListeActivity.java
@@ -0,0 +1,20 @@
+package be.bxl.formation.demo_03_liste;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import java.util.ArrayList;
+
+public class DemoListeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_demo_liste);
+
+
+
+
+    }
+}
---
+++ app/src/main/java/be/bxl/formation/demo_03_liste/MainActivity.java
@@ -0,0 +1,42 @@
+package be.bxl.formation.demo_03_liste;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private Button btnListe, btnReclycer;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btnListe = findViewById(R.id.btn_main_liste);
+        btnReclycer = findViewById(R.id.btn_main_recyclerview);
+
+        btnListe.setOnClickListener(this);
+        btnReclycer.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.btn_main_liste:
+                openListActivity();
+                break;
+            case R.id.btn_main_recyclerview:
+                throw new RuntimeException("Pas encore fait :( ");
+        }
+    }
+
+    private void openListActivity() {
+        Intent intentList = new Intent(getApplicationContext(), DemoListeActivity.class);
+        startActivity(intentList);
+    }
+}
